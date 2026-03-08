dcca80fd00336b8e329f3f041d7f05b4c7db816e
Sabbir Hosain <contact.sabbirhosain@gmail.com>
2026-03-08 16:30:17 +0600

    update

--- src/pages/AdmissionPage.jsx
+++ src/pages/AdmissionPage.jsx
@@ -2,6 +2,7 @@ import TableAdmissionComponent from '../components/admission/TableAdmissionCompo
 import { useAdmissionContext } from '../context/AdmissionContext'
 import { useCommonContext } from '../context/CommonContext'
 import { MdFormatListBulletedAdd } from 'react-icons/md'
+import { LuListFilter } from "react-icons/lu";
 import WebLayout from '../layout/WebLayout'
 import { Link } from 'react-router-dom'
 import Select from 'react-select'
@@ -22,63 +23,76 @@ const AdmissionPage = () => {
                     <h4 className='table_name_title'>Admission List</h4>
                     <Link to='/student/admission/create' className='btn btn-outline-primary btn-sm rounded-0'><MdFormatListBulletedAdd /></Link>
                 </div>
-                <div className="row bg-light shadow-sm p-2">
-                    <div className="col-md-2 mb-1 mb-sm-0">
-                        <div className='w-100'>
-                            <input type="date" onChange={(event) => updateAdmissionState({ from_date: event.target.value })} className="form-control rounded-0" />
-                        </div>
+
+                <div className="d-flex align-items-center justify-content-end gap-2 px-3 bg-light shadow-sm py-2">
+                    <div className='search-box'>
+                        <input type="search" onChange={(event) => updateAdmissionState({ search: event.target.value })} className="form-control rounded-0" placeholder="Search Hear..." />
                     </div>
-                    <div className="col-md-2 mb-1 mb-sm-0">
-                        <div className='w-100'>
-                            <input type="date" onChange={(event) => updateAdmissionState({ to_date: event.target.value })} className="form-control rounded-0" />
-                        </div>
-                    </div>
-                    <div className="col-md-2 mb-1 mb-sm-0">
-                        <Select
-                            options={semester.options}
-                            value={semester.options_value}
-                            onChange={(value) => { updateSemesterState({ options_value: value }) }}
-                            onInputChange={(input) => updateSemesterState({ search: input })}
-                            isLoading={semester.isLoading}
-                            placeholder={semester.isLoading ? "Loading..." : "Select Semester"}
-                            isClearable={true}
-                            styles={customStyles}
-                            maxMenuHeight={300}
-                            required
-                        />
-                    </div>
-                    <div className="col-md-2 mb-1 mb-sm-0">
-                        <Select
-                            options={section.options}
-                            value={section.options_value}
-                            onChange={(value) => { updateSectionState({ options_value: value }) }}
-                            onInputChange={(input) => updateSectionState({ search: input })}
-                            isLoading={section.isLoading}
-                            placeholder={section.isLoading ? "Loading..." : "Select Section"}
-                            isClearable={true}
-                            styles={customStyles}
-                            maxMenuHeight={300}
-                            required
-                        />
-                    </div>
-                    <div className="col-md-2 mb-1 mb-sm-0">
-                        <Select
-                            options={department.options}
-                            value={department.options_value}
-                            onChange={(value) => { updateDepartmentState({ options_value: value }) }}
-                            onInputChange={(input) => updateDepartmentState({ search: input })}
-                            isLoading={department.isLoading}
-                            placeholder={department.isLoading ? "Loading..." : "Select Department"}
-                            isClearable={true}
-                            styles={customStyles}
-                            maxMenuHeight={300}
-                            required
-                        />
-                    </div>
-                    <div className="col-md-2">
-                        <div className='w-100'><input type="search" onChange={(event) => updateAdmissionState({ search: event.target.value })} className="form-control rounded-0" placeholder="Search Hear..." /></div>
+
+                    <div className=''>
+                        <button className="btn btn-outline-primary btn-sm rounded-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><LuListFilter /></button>
                     </div>
                 </div>
+
+                <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title" id="offcanvasRightLabel">Admission List Filter</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" />
+                    </div>
+                    <div className="offcanvas-body">
+                        <div className="d-flex flex-column gap-2">
+                            <div className='w-100'>
+                                <input type="date" onChange={(event) => updateAdmissionState({ from_date: event.target.value })} className="form-control rounded-0" />
+                            </div>
+                            <div className='w-100'>
+                                <input type="date" onChange={(event) => updateAdmissionState({ to_date: event.target.value })} className="form-control rounded-0" />
+                            </div>
+                            <div className="w-100">
+                                <Select
+                                    options={semester.options}
+                                    value={semester.options_value}
+                                    onChange={(value) => { updateSemesterState({ options_value: value }) }}
+                                    onInputChange={(input) => updateSemesterState({ search: input })}
+                                    isLoading={semester.isLoading}
+                                    placeholder={semester.isLoading ? "Loading..." : "Select Semester"}
+                                    isClearable={true}
+                                    styles={customStyles}
+                                    maxMenuHeight={300}
+                                    required
+                                />
+                            </div>
+                            <div className='w-100'>
+                                <Select
+                                    options={department.options}
+                                    value={department.options_value}
+                                    onChange={(value) => { updateDepartmentState({ options_value: value }) }}
+                                    onInputChange={(input) => updateDepartmentState({ search: input })}
+                                    isLoading={department.isLoading}
+                                    placeholder={department.isLoading ? "Loading..." : "Select Department"}
+                                    isClearable={true}
+                                    styles={customStyles}
+                                    maxMenuHeight={300}
+                                    required
+                                />
+                            </div>
+                            <div className="w-100">
+                                <Select
+                                    options={section.options}
+                                    value={section.options_value}
+                                    onChange={(value) => { updateSectionState({ options_value: value }) }}
+                                    onInputChange={(input) => updateSectionState({ search: input })}
+                                    isLoading={section.isLoading}
+                                    placeholder={section.isLoading ? "Loading..." : "Select Section"}
+                                    isClearable={true}
+                                    styles={customStyles}
+                                    maxMenuHeight={300}
+                                    required
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div className='pt-2 pb-5'>
                     <TableAdmissionComponent />
                 </div>
